726da23b3a8970732d99aac977e778bc2ab0988f
LionsChen <chenlions@163.com>
2017-11-17 17:03:55 +0800

    回话管理类

---
+++ server/src/main/java/ustc/sse/server/SessionManager.java
@@ -0,0 +1,57 @@
+package ustc.sse.server;
+
+import org.apache.mina.core.session.IoSession;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * session管理类，用于处理保存session
+ * 单例模式，一个服务器只需要一个session管理对象
+ * created by chenhanping
+ * Designer:chenhanping
+ * Date:2017/11/17
+ * Time:下午3:48
+ */
+public class SessionManager {
+
+    //存放session的字典，用IoSession 的id作为key
+    private static Map<Long, IoSession> sessions = new ConcurrentHashMap<Long, IoSession>();
+    private static SessionManager manager = null;
+
+    private SessionManager(){ }
+
+    public static SessionManager getInstance(){
+        if (manager == null){
+            synchronized (SessionManager.class){
+                if (manager == null){
+                    manager = new SessionManager();
+                }
+            }
+        }
+        return manager;
+    }
+
+    /**
+     * 向指定客户机发送消息
+     * @param id
+     * @param msg
+     * @return
+     */
+    public void write(Long id,Object msg){
+        IoSession session = sessions.get(id);
+        session.write(msg);
+    }
+
+    /**
+     * 移除指定session
+     * @param session
+     */
+    public void remove(IoSession session){
+        sessions.remove(session);
+    }
+
+    public void add(Long id, IoSession session){
+        sessions.put(id,session);
+    }
+}
