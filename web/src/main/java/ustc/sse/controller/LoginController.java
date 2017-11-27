7f9544fdb1a326c2d6c41d4c964334cc281dc749
githubjijing <1196213374@qq.com>
2017-11-27 19:59:14 +0800

    登录和退出完成逻辑，需要重新编写登录跳转页面

--- web/src/main/java/ustc/sse/controller/LoginController.java
+++ web/src/main/java/ustc/sse/controller/LoginController.java
@@ -1,17 +1,48 @@
 package ustc.sse.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import ustc.sse.tools.dao.UserDao;
+import ustc.sse.tools.entity.UserEntity;
+import ustc.sse.tools.mapper.UserMapper;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
- * created by chenhanping
- * Designer:chenhanping
- * Date:2017/11/5
- * Time:下午5:04
+ * 作者：陈志国
+ * 时间：2017/11/26
  */
-
 @Controller
 public class LoginController {
-    public void login(){
+
+
+
+   @RequestMapping("/LoginIn")
+   @ResponseBody
+    public String checkLogin(HttpServletRequest request, HttpSession session) throws Exception{
+       String username = request.getParameter("username");
+       String password = request.getParameter("password");
+
+       UserDao dao = new UserDao();
+
+       UserEntity user = dao.queryUserByNameandPwd(username,password);
+       System.out.println(user);
+       if (user!= null){
+           session.setAttribute("user",user);
+           return "1";
+       }else{
+           return "-1";
+       }
 
     }
+
+    @RequestMapping("/LoginOut")
+    public String loginOut(HttpSession session) throws Exception{
+       session.removeAttribute("user");
+       return "login.jsp";
+    }
+
 }
